350f7976d9358674aae1a832bdec882ffc5ad1bf
Joseph Sanjaya, S.T., M.Kom <sanjayajosep@gmail.com>
2021-10-03 12:21:06 +0700

    init project

---
+++ app/src/main/java/com/workspaces/ziip/zotero/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.workspaces.ziip.zotero
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
